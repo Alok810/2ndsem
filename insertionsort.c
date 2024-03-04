9c662e5fd886edca50e4451f268db775cba356f0
Alok kumar <154756272+Alok810@users.noreply.github.com>
2024-03-04 16:03:02 +0530

    insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -1,8 +1,4 @@
-// Insertion sort in C
-
 #include <stdio.h>
-
-// Function to print an array
 void printArray(int array[], int size) {
   for (int i = 0; i < size; i++) {
     printf("%d ", array[i]);
@@ -14,10 +10,6 @@ void insertionSort(int array[], int size) {
   for (int step = 1; step < size; step++) {
     int key = array[step];
     int j = step - 1;
-
-    // Compare key with each element on the left of it until an element smaller than
-    // it is found.
-    // For descending order, change key<array[j] to key>array[j].
     while (key < array[j] && j >= 0) {
       array[j + 1] = array[j];
       --j;
